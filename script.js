be88be342576d3bc4496931fadb92ecd36094ef5
Bagusadi99 <bagusadisurya0000@gmail.com>
2024-05-02 01:16:02 +0700

    progres ke 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const logregBox = document.querrySelection('.logreg-box');
+const loginLink = document.querrySelection('.login-link');
+const registerLink = document.querrySelection('.register-box');
+
+registerLink.addEventListener('click', () => {
+    logregBox.classList.add('active');
+});
